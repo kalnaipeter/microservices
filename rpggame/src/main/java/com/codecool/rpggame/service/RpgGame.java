a6dd414809aa540d319baf82c34f7c2c790b7774
kalnaipeter <kalnaipeter@gmail.com>
2020-07-17 00:09:31 +0200

    first commit

---
+++ rpggame/src/main/java/com/codecool/rpggame/service/RpgGame.java
@@ -0,0 +1,22 @@
+package com.codecool.rpggame.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RpgGame {
+
+    @Autowired
+    private RollServiceCaller serviceCaller;
+
+    public String evaluateCombat() {
+        int heroDmg = serviceCaller.rollForHero();
+        int monsterDmg = serviceCaller.rollForMonster();
+
+        String result = heroDmg + " vs " + monsterDmg;
+
+        return heroDmg > monsterDmg ? "Hero won! " + result : "Monster won! " + result;
+    }
+}
